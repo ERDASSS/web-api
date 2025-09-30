11e1e2740181c20641ee563ec411c6f0daf6b104
ERDAS <com123465@mail.ru>
2025-09-30 16:34:31 +0500

    1

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 
@@ -8,15 +9,37 @@ namespace WebApi.MinimalApi.Controllers;
 [ApiController]
 public class UsersController : Controller
 {
+    
+    private readonly IUserRepository userRepository;
+    private readonly IMapper mapper;
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
     }
-
+    
+    [Produces("application/json", "application/xml")]
     [HttpGet("{userId}")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        throw new NotImplementedException();
+        var user = userRepository.FindById(userId);
+        
+        if (user == null)
+            return NotFound();
+
+        // var userDto = new UserDto
+        // {
+        //     Id = user.Id,
+        //     CurrentGameId = user.CurrentGameId,
+        //     FullName = $"{user.LastName} {user.FirstName}",
+        //     Login = user.Login,
+        //     GamesPlayed = user.GamesPlayed
+        // };
+        
+        var userDto = mapper.Map<UserDto>(user);
+        
+        return Ok(userDto);
     }
 
     [HttpPost]
