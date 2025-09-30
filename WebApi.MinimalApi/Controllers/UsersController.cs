de1ca64f6389139e59e7908e1d52a55e29da95a2
ERDAS <com123465@mail.ru>
2025-09-30 17:19:55 +0500

    3

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -47,4 +47,34 @@ public class UsersController : Controller
     {
         throw new NotImplementedException();
     }
+
+    [HttpPut("{userId}")]
+    public IActionResult UpdateUser([FromRoute] Guid userId, [FromBody] UserDto user)
+    {
+        var existingUser = userRepository.FindById(userId);
+        var userLastName = user.FullName.Split(' ')[0];
+        var userFirstName = user.FullName.Split(' ')[1];
+        
+        if (existingUser == null)
+        {
+            var userEntity = mapper.Map<UserEntity>(user);
+            userEntity.LastName = userLastName;
+            userEntity.FirstName = userFirstName;
+            // var newUser = new UserEntity(userId, user.Login, userLastName, userFirstName, user.GamesPlayed, user.CurrentGameId);
+            userRepository.UpdateOrInsert(userEntity, out var isInserted);
+            
+            var updatedUserDto = mapper.Map<UserDto>(userEntity);
+            return CreatedAtAction(nameof(GetUserById), new { userId = userId }, updatedUserDto);
+        }
+        else
+        {
+            mapper.Map(user, existingUser);
+            existingUser.LastName = userLastName;
+            existingUser.FirstName = userFirstName;
+            userRepository.UpdateOrInsert(existingUser, out var isInserted);
+            
+            var updatedUserDto = mapper.Map<UserDto>(existingUser);
+            return Ok(updatedUserDto);
+        }
+    }
 }
